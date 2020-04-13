0aa410a58358839ac3a0214a49f4e4d658be6d19
Srinivas Joshi <32826109+joshi2403@users.noreply.github.com>
2020-04-13 10:33:45 +0530

    Add files via upload

---
+++ file_upload_session.py
@@ -0,0 +1,20 @@
+from office365.runtime.auth.UserCredential import UserCredential
+from settings import settings
+from office365.sharepoint.client_context import ClientContext
+
+
+def print_upload_progress(offset):
+    print("Uploaded '{0}' bytes...".format(offset))
+
+
+if __name__ == '__main__':
+    site_url = settings['url']
+    ctx = ClientContext.connect_with_credentials(site_url, UserCredential(settings['user_credentials']['username'],
+                                                                          settings['user_credentials']['password']))
+    size_1Mb = 1000000
+    local_path = "../../tests/data/big_buck_bunny.mp4"
+    target_url = "/Shared Documents"
+    result_file = ctx.web.get_folder_by_server_relative_url(target_url) \
+        .files.create_upload_session(local_path, size_1Mb, print_upload_progress)
+    ctx.execute_query()
+    print('File {0} has been uploaded successfully'.format(result_file.properties['ServerRelativeUrl']))
